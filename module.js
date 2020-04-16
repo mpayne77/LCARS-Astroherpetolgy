01f7c7060ff7744b29df6fa7d4cf7ab475db097f
Matt <mpayne77@gmail.com>
2020-04-15 20:52:45 -0500

    Refactoring, added comments, changed all var to let, removed unnecessary updateItems() calls

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
-var today = new Date();
-var lightStatus = 'UNKNOWN';
-var onTime = 0;
-var offTime = 1;
+let today = new Date();
+let lightStatus = 'UNKNOWN';
+let onTime = 0;
+let offTime = 1;
 let mode = 'NORMAL';
 let tempLight = 0.0;
 let rhLight = 0.0;
@@ -12,22 +12,22 @@ let soilVWC = 0.0
 let uvIntensity = 0.0
 let tzAdjust = -5;
 
-var topFrame1 = LCARS.create({type: 'bar', color:'bg-orange-3', label:'LCARS', id: 'topFrame1'});
-var topFrame2 = LCARS.create({type: 'elbow', color:'bg-orange-3', label:'ACCESS', id: 'topFrame2'});
-var topFrame3 = LCARS.create({type: 'bar', color:'bg-orange-4', id: 'topFrame3'});
-var topFrame4 = LCARS.create({type: 'bar', color:'bg-orange-3', label: 'STARDATE:', id: 'topFrame4'});
-var topFrame5 = LCARS.create({type: 'bar', color:'bg-orange-4', id: 'stardate', label: stardate(), id: 'topFrame5'});
-var pageTitle = LCARS.create({type: 'title', color:'bg-orange-3', text: 'ASTROHERPETOLOGY', id: 'pageTitle'});
-var bottomFrame1 = LCARS.create({type: 'elbow', color:'bg-orange-3', direction: 'top-left', id: 'bottomFrame1'});
-var bottomFrame2 = LCARS.create({type: 'bar', color:'bg-orange-3', id: 'bottomFrame2'});
-var bottomFrame3 = LCARS.create({type: 'bar', color:'bg-orange-3', label: 'TIME:', id: 'bottomFrame3'});
-var bottomFrame4 = LCARS.create({type: 'bar', color:'bg-orange-3', id: 'startime', label: startime(), id: 'bottomFrame4'});
-var diagnosticButton = LCARS.create({type: 'button', color:'bg-purple-5', label:'SYSTEM\nDIAGNOSTIC', id: 'diagnosticButton'});
-var timerAdjustButton = LCARS.create({type: 'button', color:'bg-purple-4', label:'TIMER\nADJUST', id: 'timerAdjustButton'});
-var timerOverrideButton = LCARS.create({type: 'button', color:'bg-orange-4', label:'TIMER\nOVERRIDE', id: 'timerOverrideButton'});
-var turtlePicture = LCARS.create({type: 'img', src: 'tortoise.png', id: 'turtlePicture',});
+let topFrame1 = LCARS.create({type: 'bar', color:'bg-orange-3', label:'LCARS', id: 'topFrame1'});
+let topFrame2 = LCARS.create({type: 'elbow', color:'bg-orange-3', label:'ACCESS', id: 'topFrame2'});
+let topFrame3 = LCARS.create({type: 'bar', color:'bg-orange-4', id: 'topFrame3'});
+let topFrame4 = LCARS.create({type: 'bar', color:'bg-orange-3', label: 'STARDATE:', id: 'topFrame4'});
+let topFrame5 = LCARS.create({type: 'bar', color:'bg-orange-4', id: 'stardate', label: stardate(), id: 'topFrame5'});
+let pageTitle = LCARS.create({type: 'title', color:'bg-orange-3', text: 'ASTROHERPETOLOGY', id: 'pageTitle'});
+let bottomFrame1 = LCARS.create({type: 'elbow', color:'bg-orange-3', direction: 'top-left', id: 'bottomFrame1'});
+let bottomFrame2 = LCARS.create({type: 'bar', color:'bg-orange-3', id: 'bottomFrame2'});
+let bottomFrame3 = LCARS.create({type: 'bar', color:'bg-orange-3', label: 'TIME:', id: 'bottomFrame3'});
+let bottomFrame4 = LCARS.create({type: 'bar', color:'bg-orange-3', id: 'localTime', label: localTime(), id: 'bottomFrame4'});
+let diagnosticButton = LCARS.create({type: 'button', color:'bg-purple-5', label:'SYSTEM\nDIAGNOSTIC', id: 'diagnosticButton'});
+let timerAdjustButton = LCARS.create({type: 'button', color:'bg-purple-4', label:'TIMER\nADJUST', id: 'timerAdjustButton'});
+let timerOverrideButton = LCARS.create({type: 'button', color:'bg-orange-4', label:'TIMER\nOVERRIDE', id: 'timerOverrideButton'});
+let turtlePicture = LCARS.create({type: 'img', src: 'tortoise.png', id: 'turtlePicture',});
 
-var pictureBracket = LCARS.create({type: 'defaultBracket', namespace: 'sdk', id:'pictureBracket',
+let pictureBracket = LCARS.create({type: 'defaultBracket', namespace: 'sdk', id:'pictureBracket',
 coloring: {
     elbow: 'bg-orange-5',
     column1: ['bg-orange-4', 'bg-orange-3', 'bg-orange-4'],
@@ -38,46 +38,46 @@ coloring: {
   },
 });
 
-var tempLightStatus = LCARS.create({type:'complexButton', id: 'tempLightStatus', children: [
+let tempLightStatus = LCARS.create({type:'complexButton', id: 'tempLightStatus', children: [
   {type: 'button', version: 'round-left', color: 'bg-purple-3', label: 'TEMP\n(LIGHT)', id: 'tempLightButton'}, 
   {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'tempLightText'}, 
   {type: 'bar', version: 'round-right', color: 'bg-purple-3', id: 'tempLightCap'}
 ]});
 
-var rhLightStatus = LCARS.create({type:'complexButton', id: 'rhLightStatus', children: [
+let rhLightStatus = LCARS.create({type:'complexButton', id: 'rhLightStatus', children: [
   {type: 'button', version: 'round-left', color: 'bg-purple-4', label: 'RH\n(LIGHT)', id: 'rhLightButton'}, 
   {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'rhLightText'}, 
   {type: 'bar', version: 'round-right', color: 'bg-purple-4', id: 'rhLightCap'}
 ]});
 
-var tempShadeStatus = LCARS.create({type:'complexButton', id: 'tempShadeStatus', children: [
+let tempShadeStatus = LCARS.create({type:'complexButton', id: 'tempShadeStatus', children: [
   {type: 'button', version: 'round-left', color: 'bg-purple-3', label: 'TEMP\n(SHADE)', id: 'tempShadeButton'}, 
   {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'tempShadeText'}, 
   {type: 'bar', version: 'round-right', color: 'bg-purple-3', id: 'tempShadeCap'}
 ]});
 
-var rhShadeStatus = LCARS.create({type:'complexButton', id: 'rhShadeStatus', children: [
+let rhShadeStatus = LCARS.create({type:'complexButton', id: 'rhShadeStatus', children: [
   {type: 'button', version: 'round-left', color: 'bg-purple-4', label: 'RH\n(SHADE)', id: 'rhShadeButton'}, 
   {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'rhShadeText'}, 
   {type: 'bar', version: 'round-right', color: 'bg-purple-4', id: 'rhShadeCap'}
 ]});
 
-var soilVWCStatus = LCARS.create({type:'complexButton', id: 'soilVWCStatus', children: [
+let soilVWCStatus = LCARS.create({type:'complexButton', id: 'soilVWCStatus', children: [
   {type: 'button', version: 'round-left', color: 'bg-purple-3', label: 'SOIL\nHUMIDITY', id: 'soilVWCButton'}, 
   {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'soilVWCText'}, 
   {type: 'bar', version: 'round-right', color: 'bg-purple-3', id: 'soilVWCCap'}
 ]});
 
-var uvLightStatus = LCARS.create({type:'complexButton', id: 'uvLightStatus', children: [
-  {type: 'button', version: 'round-left', color: 'bg-purple-4', label: 'UV\nINTENSITY', id: 'uvLightButton'}, 
-  {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'uvLightText'}, 
-  {type: 'bar', version: 'round-right', color: 'bg-purple-4 ', id: 'uvLightCap'}
+let uvIntensityStatus = LCARS.create({type:'complexButton', id: 'uvIntensityStatus', children: [
+  {type: 'button', version: 'round-left', color: 'bg-purple-4', label: 'UV\nINTENSITY', id: 'uvIntensityButton'}, 
+  {type: 'title', color: 'bg-grey-1', text: '--.-', id: 'uvIntensityText'}, 
+  {type: 'bar', version: 'round-right', color: 'bg-purple-4 ', id: 'uvIntensityCap'}
 ]});
 
-var lightPowerToggle = LCARS.create({type: 'button', version: 'round', color: 'bg-red-1', label: '', id: 'lightPowerToggle'});
-var lightOverrideText = LCARS.create({type: 'text', color: 'bg-purple-1', text: lightOverrideMessage, id: 'lightOverrideText'});
+let lightPowerToggle = LCARS.create({type: 'button', version: 'round', color: 'bg-red-1', label: '', id: 'lightPowerToggle'});
+let lightOverrideText = LCARS.create({type: 'text', color: 'bg-purple-1', text: lightOverrideMessage, id: 'lightOverrideText'});
 
-var timerSetGraph = LCARS.create({type: 'wrapper', id: 'timerSetGraph', children: [
+let timerSetGraph = LCARS.create({type: 'wrapper', id: 'timerSetGraph', children: [
 	{type: 'elbow', direction: 'top-left', version: 'vertical', size: 'small', color:'bg-orange-4', id: 'timerFrameTopLeft'},	
 	{type: 'elbow', direction: 'bottom-left', version: 'vertical', size: 'small', color:'bg-orange-4', id: 'timerFrameBottomLeft'},	
 	{type: 'elbow', direction: 'bottom-right', version: 'vertical', size: 'small', color:'bg-orange-4', id: 'timerFrameBottomRight'},	
@@ -176,7 +176,7 @@ var timerSetGraph = LCARS.create({type: 'wrapper', id: 'timerSetGraph', children
   ]},
 ]});
 
-var timerSet = LCARS.create({type: 'row', flexc: 'h', id: 'timerSet', children: [
+let timerSet = LCARS.create({type: 'row', flexc: 'h', id: 'timerSet', children: [
   {type: 'column', flexc: 'v', children: [
     {type: 'button', version: 'round', color: 'bg-green-2', label: 'LIGHT ON +', id: 'onInc'},
     {type: 'title', color: 'bg-grey-1', text: '00:00', id: 'onTime'},
@@ -189,7 +189,7 @@ var timerSet = LCARS.create({type: 'row', flexc: 'h', id: 'timerSet', children: 
   ]},
 ]});
 
-var infoText = LCARS.create({type: 'column', flex: 'v', id: 'infoText', children: [
+let infoText = LCARS.create({type: 'column', flex: 'v', id: 'infoText', children: [
   {type: 'row', flex: 'h', children: [
     {type: 'oval', color: 'bg-purple-1', size: 'small'},
     {type: 'text', color: 'bg-purple-2', text: textInfo1},
@@ -220,7 +220,7 @@ var infoText = LCARS.create({type: 'column', flex: 'v', id: 'infoText', children
   ]},
 ]});
 
-var diagnosticFrame = LCARS.create({type: 'column', flex: 'v', id: 'diagnosticFrame', children: [
+let diagnosticFrame = LCARS.create({type: 'column', flex: 'v', id: 'diagnosticFrame', children: [
   {type: 'row', flex: 'h', id: 'tzAdjust', children: [
     {type: 'button', version: 'round-left', color: 'bg-purple-1', text: '-', id: 'tzDecButton'},
     {type: 'text', color: 'bg-grey-1', text: '--', id: 'tzAdjustText'},
@@ -279,7 +279,7 @@ $(document).ready(function(){
   $('body').append((tempShadeStatus).dom);
   $('body').append((rhShadeStatus).dom);
   $('body').append((soilVWCStatus).dom);
-  $('body').append((uvLightStatus).dom);
+  $('body').append((uvIntensityStatus).dom);
   $('body').append((lightPowerToggle).dom);
   $('#lightPowerToggle').hide();
   $('body').append((lightOverrideText).dom);
@@ -318,11 +318,18 @@ $(document).ready(function(){
 
 });
 
+// Requests JSON from Arduino and updates all UI elements accordingly
 function updateItems() {
-  bottomFrame4.set('label', startime());
+  
+  topFrame5.set('label', stardate());
+  bottomFrame4.set('label', localTime());
+  
   setTimeBarPostion();
+  
   $.get('http://192.168.1.219/', function(data) {
-    let jsonData = JSON.parse(data);
+    
+  // Collect and parse JSON from Arduino
+  let jsonData = JSON.parse(data);
     console.log(jsonData);
     lightStatus = jsonData.lightStatus;
     mode = jsonData.mode;
@@ -341,20 +348,18 @@ function updateItems() {
     if (mode != 'DIAGNOSTIC') {
       tzAdjust = jsonData.tzAdjust;
     }
-
   });
 
   if (lightStatus === 'ON') {
-    uvLightStatus.set('state', 'blink');
-    uvLightStatus.set('colors', ['bg-blue-1', 'bg-grey-1', 'bg-blue-1']);
+    uvIntensityStatus.set('state', 'blink');
+    uvIntensityStatus.set('colors', ['bg-blue-1', 'bg-grey-1', 'bg-blue-1']);
     tempLightStatus.set('state', 'blink');
     tempLightStatus.set('colors', ['bg-blue-1', 'bg-grey-1', 'bg-blue-1']);
     lightPowerToggle.set('label', 'TURN LIGHT OFF');
     lightPowerToggle.set('color', 'bg-red-1');
-  }
-  else {
-    uvLightStatus.set('state', null);
-    uvLightStatus.set('colors', ['bg-purple-4', 'bg-grey-1', 'bg-purple-4']);
+  } else {
+    uvIntensityStatus.set('state', null);
+    uvIntensityStatus.set('colors', ['bg-purple-4', 'bg-grey-1', 'bg-purple-4']);
     tempLightStatus.set('state', null);
     tempLightStatus.set('colors', ['bg-purple-3', 'bg-grey-1', 'bg-purple-3']);
     lightPowerToggle.set('label', 'TURN LIGHT ON');
@@ -435,39 +440,47 @@ function updateItems() {
   }
 
   if (tempLight > 0) {
-    $('#tempLightText').html(tempLight);
+    LCARS.active.tempLightText.set('text', (tempLight.toFixed(1)).toString());
   } else {
-    $('#tempLightText').html('--.-');
+    LCARS.active.tempLightText.set('text', '--.-');
   }
   
   if (rhLight > 0) {
-    $('#rhLightText').html(rhLight);
+    LCARS.active.rhLightText.set('text', (rhLight.toFixed(1)).toString());
   } else {
-    $('#rhLightText').html('--.-');
+    LCARS.active.rhLightText.set('text', '--.-');
+    
   }
   
   if (tempShade > 0) {
-    $('#tempShadeText').html(tempShade);
+    LCARS.active.tempShadeText.set('text', (tempShade.toFixed(1)).toString());
   } else {
-    $('#tempShadeText').html('--.-');
+    LCARS.active.tempShadeText.set('text', '--.-');
   }
   
   if (rhShade > 0) {
-    $('#rhShadeText').html(rhShade);
+    LCARS.active.rhShadeText.set('text', (rhShade.toFixed(1)).toString());
   } else {
-    $('#rhShadeText').html('--.-');
+    LCARS.active.rhShadeText.set('text', '--.-');
   }
 
   if (soilVWC > 1) {
-    $('#soilVWCText').html(soilVWC.toFixed(1));
+    LCARS.active.soilVWCText.set('text', (soilVWC.toFixed(1)).toString());
   } else {
-    $('#soilVWCText').html('--.-');
+    LCARS.active.soilVWCText.set('text', '--.-');
   }
   
+  if (uvIntensity > 1) {
+    LCARS.active.soilVWCText.set('text', (soilVWC.toFixed(1)).toString());
+  } else {
+    LCARS.active.soilVWCText.set('text', '--.-');
+  }
+
   LCARS.active.tzAdjustText.set('text', tzAdjust.toString());
   
 }
 
+// Toggles between normal and diagnostic mode
 function diagnosticButtonClick() {
   const beep = document.getElementById('beep2');
   //beep.play();
@@ -478,6 +491,7 @@ function diagnosticButtonClick() {
   }
 }
 
+// Toggles between normal and timer override mode
 function timerOverrideButtonClick() {
   const beep = document.getElementById('beep1');
   //beep.play();
@@ -486,9 +500,11 @@ function timerOverrideButtonClick() {
   } else {
     $.get('http://192.168.1.219/NORMAL');
   }
-  updateItems();
 }
 
+// Toggles between normal and timer adjust mode
+// When exiting timer adjust mode back to normal mode, request to adjust timer thresholds
+// is sent to the Arduino
 function timerAdjustButtonClick() {
   const beep = document.getElementById('beep1');
   //beep.play();
@@ -497,12 +513,13 @@ function timerAdjustButtonClick() {
   } else {
     $.get('http://192.168.1.219/' + onTime.toString() + '-' + offTime.toString() + '-SETTIMER');
   }
-  updateItems();
 }
 
+
+// Sends request to Arduino to reset RTC with specified offset for local time zone.
+// To avoid needing to parse negative and positive numbers in the URL request string, I'm
+// adding 12 to the time zone before sending, then removing that 12 on the Arduino side
 function tzSetButtonClick() {
-  // to avoid needing to parse negative and positive numbers in the string received by the Arduino
-  // I'm adding 12 to the time zone before sending. The extra 12 is then subtracted off on the Arduino side
   let tzOffset = tzAdjust + 12;
   let tzOffsetStr = tzOffset.toString();
   if (tzOffset < 10) {
@@ -511,111 +528,117 @@ function tzSetButtonClick() {
   $.get('http://192.168.1.219/' + tzOffsetStr + '-SETCLOCK');
 }
 
+// Pulls local time from network and returns date string in YYYY.MM.DD format
 function stardate() {
-  var today = new Date;
-  var sdYear = today.getFullYear();
-  var sdMonth = today.getMonth() + 1;
-  var sdDate = today.getDate();
-  var strSDyear = sdYear.toString();
-  var strSDmonth = sdMonth.toString();
+  let today = new Date;
+  let sdYear = today.getFullYear();
+  let sdMonth = today.getMonth() + 1;
+  let sdDate = today.getDate();
+  let strSDyear = sdYear.toString();
+  let strSDmonth = sdMonth.toString();
   if (sdMonth < 10) {
     strSDmonth = '0' + strSDmonth;
   }
-  var strSDdate = sdDate.toString();
+  let strSDdate = sdDate.toString();
   if (sdDate < 10) {
     strSDdate = '0' + strSDdate;
   }
   return strSDyear + '.' + strSDmonth + '.' + strSDdate;
 }
 
-function startime() {
-  var today = new Date;
-  var sdHours = today.getHours();
-  var sdMinutes = today.getMinutes();
-  var sdSeconds = today.getSeconds();
-  var strSDhours = sdHours.toString();
+// Pulls local time from network and returns string in HH:MM:SS format
+function localTime() {
+  let today = new Date;
+  let sdHours = today.getHours();
+  let sdMinutes = today.getMinutes();
+  let sdSeconds = today.getSeconds();
+  let strSDhours = sdHours.toString();
   if (sdHours < 10) {
     strSDhours = '0' + strSDhours;
   }
-  var strSDminutes = sdMinutes.toString();
+  let strSDminutes = sdMinutes.toString();
   if (sdMinutes < 10) {
     strSDminutes = '0' + strSDminutes;
   }
-  var strSDseconds = sdSeconds.toString();
+  let strSDseconds = sdSeconds.toString();
   if (sdSeconds < 10) {
     strSDseconds = '0' + strSDseconds;
   }
-
   return strSDhours + ':' + strSDminutes + ':' + strSDseconds;
 }
 
+// Draws bar representing current local time on timer adjust bar graph
 function setTimeBarPostion() {
-  var today = new Date;
-  var minutesElapsed = 60*today.getHours() + today.getMinutes();
-  var proportionElapsed = minutesElapsed / (24*60);
-  var pctPosition = 10 + proportionElapsed*83;
+  let today = new Date;
+  let minutesElapsed = 60*today.getHours() + today.getMinutes();
+  let proportionElapsed = minutesElapsed / (24*60);
+  let pctPosition = 10 + proportionElapsed*83;
   $('#currentTimeBar').css('left', pctPosition.toString() + '%');
 }
 
+// Toggles light on and off
 function lightPowerToggleClick () {
   const beep = document.getElementById('beep4');
   //beep.play();
   if (lightStatus === 'ON') {
     $.get('http://192.168.1.219/LIGHT-OFF');
-    updateItems();
   } else {
     $.get('http://192.168.1.219/LIGHT-ON');
-    updateItems();
   }
 }
 
+// Increments on time for light timer
 function onIncClick () {
   if (onTime < 96 && onTime < offTime-1) {onTime++;}
   $('#onTime').html(parseTime(onTime));
   updateTimerBar(onTime, offTime);
 }
 
+// Decrements on time for light timer
 function onDecClick () {
   if (onTime > 0) {onTime--;}
   $('#onTime').html(parseTime(onTime));
   updateTimerBar(onTime, offTime);
 }
 
+// Increments off time for light timer
 function offIncClick () {
   if (offTime < 24*4) {offTime++;}
   $('#offTime').html(parseTime(offTime));
   updateTimerBar(onTime, offTime);
 }
 
+// Decrements off time for light timer
 function offDecClick () {
   if (offTime > onTime+1) {offTime--;}
   $('#offTime').html(parseTime(offTime));
   updateTimerBar(onTime, offTime);
 }
 
+// Converts time index into string in HH:MM format
 function parseTime (timeIndex) {
-  var hours = Math.floor(timeIndex/4);
-  var minutes = Math.floor(timeIndex*15 - hours*60);
-  var strHours = hours.toString();
+  let hours = Math.floor(timeIndex/4);
+  let minutes = Math.floor(timeIndex*15 - hours*60);
+  let strHours = hours.toString();
   if (hours < 10) {
     strHours = '0' + strHours;
   }
-  var strMinutes = minutes.toString();
+  let strMinutes = minutes.toString();
   if (minutes < 10) {
     strMinutes = '0' + strMinutes;
   }
   return strHours + ':' + strMinutes;
 }
 
+// Draws bar representing time light will be powered on timer adjust bar graph
 function updateTimerBar(onTime, offTime) {
-  var startPos = 10 + onTime*83/96;
-  var barLength = (offTime-onTime)*83/96;
+  let startPos = 10 + onTime*83/96;
+  let barLength = (offTime-onTime)*83/96;
   $('#timerBar').css('left', startPos.toString() + '%');  
   $('#timerBar').css('width', barLength.toString() + '%');
 }
 
-
-
+// Increments time zone offset
 function tzDecButtonClick () {
   if (tzAdjust > -12 && tzAdjust <= 12) {
     tzAdjust--;
@@ -623,6 +646,7 @@ function tzDecButtonClick () {
   }
 }
 
+// Decrements time zone offset
 function tzIncButtonClick () {
   if (tzAdjust >= -12 && tzAdjust < 12) {
     tzAdjust++;
